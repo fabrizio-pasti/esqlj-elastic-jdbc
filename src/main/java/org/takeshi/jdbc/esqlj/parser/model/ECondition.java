f1bd66229bd057775bbe38d4758a772b8e90466a
mporcini <mporcini@gmail.com>
2021-01-30 19:31:57 +0100

    Add Condition Enumeretor

---
+++ src/main/java/org/takeshi/jdbc/esqlj/parser/model/ECondition.java
@@ -0,0 +1,13 @@
+package org.takeshi.jdbc.esqlj.parser.model;
+
+public enum ECondition {
+	Between,
+	EqualsTo,
+	GreaterThan,
+	GreaterThanEquals,
+	InExpression,
+	LikeExpression,
+	MinorThan,
+	MinorThanEquals,
+	NotEqualsTo;
+}
